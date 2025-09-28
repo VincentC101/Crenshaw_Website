6b513a511074ab0c12d0eac2499a90a2580eb9e7
VincentC101 <113321507+VincentC101@users.noreply.github.com>
2025-09-27 20:06:17 -0500

    Revert "Update"

--- crenshaw_collection/music/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Album
-
-class AlbumForm(forms.ModelForm):
-    class Meta:
-        model = Album
-        fields = ['cover', 'artist', 'title', 'rating']
